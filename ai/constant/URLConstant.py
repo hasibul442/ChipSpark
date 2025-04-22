7bb25e1d847f9f863f02cd8a7030ff62d956865c
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-04-22 22:56:36 +0600

    Add URL constants and implement health check endpoint in FastAPI application

---
+++ ai/constant/URLConstant.py
@@ -0,0 +1,5 @@
+# Define the API prefix constant
+API_PREFIX = "/api/v1"
+
+# Define the health check endpoint constant
+HEALTH_CHECK = "/health"
--- ai/main.py
+++ ai/main.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from constant.URLConstant import HEALTH_CHECK, API_PREFIX
 
 app = FastAPI()
 
+# Create a router with a prefix
+api_router = APIRouter(prefix=API_PREFIX)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello FastAPI"}
+
+@api_router.get(HEALTH_CHECK)
+async def health():
+    response = {
+        "message": "Ai Microservice is Running Ok",
+        "status": "success"
+    }
+    return response
+
+@api_router.get("/test")
+async def test():
+    response = {
+        "message": "Hello Test",
+        "status": "success"
+    }
+    return response
+
+# Include the router in the main app
+app.include_router(api_router)
